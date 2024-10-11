b58583757af85131fcc04c2f57f542c3d1d212c0
Zanchet2020 <lucasamuel040@gmail.com>
2024-10-11 14:26:25 -0300

    Tryed adding bild to optimize adding the images, but got worse performance

--- Find_the_FLAG/find_flag.go
+++ Find_the_FLAG/find_flag.go
@@ -2,84 +2,98 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/png"
-	"os"
-	"sync"
+	//"image"
+	//"image/color"
+	//"image/png"
+	//	"os"
+	//	"sync"
+	"github.com/anthonynsimon/bild/blend"
+	"github.com/anthonynsimon/bild/imgio"
 )
 
 
-func decodeImage(filename string) (image.Image, error){
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
+// func decodeImage(filename string) (image.Image, error){
+// 	f, err := os.Open(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer f.Close()
 
-	img, _, err := image.Decode(f)
-	if err != nil {
-		return nil, err
-	}
+// 	img, _, err := image.Decode(f)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return img, err
-}
+// 	return img, err
+// }
 
 
-func encodeImage(filename string, img image.Image) (error) {
-	f, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+// func encodeImage(filename string, img image.Image) (error) {
+// 	f, err := os.Create(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
 
-	return png.Encode(f, img)
-}
+// 	return png.Encode(f, img)
+// }
 
-func inner_loop(i int, height int, img1 image.Image, img2 image.Image, img_out *image.RGBA, wg *sync.WaitGroup){
-	defer wg.Done()
-	for j:= range height{
-		//var r1, r2, g1, g2, b1, b2 uint32
-		r1, g1, b1, _ := img1.At(i, j).RGBA()
-		r2, g2, b2, _ := img2.At(i, j).RGBA()
+// func inner_loop(i int, height int, img1 image.Image, img2 image.Image, img_out *image.RGBA, wg *sync.WaitGroup){
+// 	defer wg.Done()
+// 	for j:= range height{
+// 		//var r1, r2, g1, g2, b1, b2 uint32
+// 		r1, g1, b1, _ := img1.At(i, j).RGBA()
+// 		r2, g2, b2, _ := img2.At(i, j).RGBA()
 		
 		
-		// if r1 == r2 && g1 == g2 && b1 == b2{
-		// 	fmt.Println(i, j)
-		// 	fmt.Println(randomImage.At(i,j).RGBA())
-		// 	fmt.Println(encryptedImage.At(i,j).RGBA())
-		// }
-		col := color.RGBA{uint8(r2 + r1), uint8(g2 + g1), uint8(b2 + b1), 255}
-		img_out.Set(i, j, col)
-	}
-}
+// 		// if r1 == r2 && g1 == g2 && b1 == b2{
+// 		// 	fmt.Println(i, j)
+// 		// 	fmt.Println(randomImage.At(i,j).RGBA())
+// 		// 	fmt.Println(encryptedImage.At(i,j).RGBA())
+// 		// }
+// 		col := color.RGBA{uint8(r2 + r1), uint8(g2 + g1), uint8(b2 + b1), 255}
+// 		img_out.Set(i, j, col)
+// 	}
+// }
 
 func main(){
 
-	randomImage, err := decodeImage("random-image.png")
-	if err != nil{
-		fmt.Println("Failed to decode image:", err)
-	}
-	encryptedImage, err := decodeImage("encrypted1.png")
-	if err != nil{
-		fmt.Println("Failed to decode image:", err)
+	randomImage, err := imgio.Open("random-image.png")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	bounds := randomImage.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-
-	var wg sync.WaitGroup
-	wg.Add(width)
+	encryptedImage, err := imgio.Open("encrypted1.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	
-	decryptedImage := image.NewRGBA(bounds)
+	// randomImage, err := decodeImage("random-image.png")
+	// if err != nil{
+	// 	fmt.Println("Failed to decode image:", err)
+	// }
+	// encryptedImage, err := decodeImage("encrypted1.png")
+	// if err != nil{
+	// 	fmt.Println("Failed to decode image:", err)
+	// }
 
-	for i := range width{
-		// for j:= range height{
+	// bounds := randomImage.Bounds()
+	// width, height := bounds.Max.X, bounds.Max.Y
+
+	// var wg sync.WaitGroup
+	// wg.Add(width)
+	
+	// decryptedImage := image.NewRGBA(bounds)
+
+	// for i := range width{
+	// 	// for j:= range height{
 		// 	//var r1, r2, g1, g2, b1, b2 uint32
 		// 	r1, g1, b1, _ := randomImage.At(i, j).RGBA()
 		// 	r2, g2, b2, _ := encryptedImage.At(i, j).RGBA()
-
-		go inner_loop(i, height, randomImage, encryptedImage, decryptedImage, &wg)
+	output := blend.Add(randomImage, encryptedImage)
+		// go inner_loop(i, height, randomImage, encryptedImage, decryptedImage, &wg)
 		// 	// if r1 == r2 && g1 == g2 && b1 == b2{
 		// 	// 	fmt.Println(i, j)
 		// 	// 	fmt.Println(randomImage.At(i,j).RGBA())
@@ -88,14 +102,19 @@ func main(){
 		// 	col := color.RGBA{uint8(r2 + r1), uint8(g2 + g1), uint8(b2 + b1), 255}
 		// 	decryptedImage.Set(i, j, col)
 		// }
-	}
+	// }
 
-	wg.Wait()
-	
-	err = encodeImage("decoded1.png", decryptedImage)
-	if err != nil{
-		fmt.Println("Failed to save image:", err)
+	// wg.Wait()
+
+	if err := imgio.Save("decoded1.png", output, imgio.PNGEncoder()); err != nil {
+		fmt.Println(err)
+		return
 	}
+	
+	// err = encodeImage("decoded1.png", decryptedImage)
+	// if err != nil{
+	// 	fmt.Println("Failed to save image:", err)
+	// }
 
 	
 	return
--- Find_the_FLAG/go.mod
+++ Find_the_FLAG/go.mod
@@ -1,3 +1,11 @@
 module find_the_flag
 
 go 1.22.7
+
+require (
+	github.com/anthonynsimon/bild v0.14.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/image v0.18.0 // indirect
+)
